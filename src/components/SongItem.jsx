2e3d8b4de95e9e4cf4d256a4b3df0bb9561ce10b
Thomas <thomassantospallarini@gmail.com>
2025-05-09 14:02:06 -0300

    Component: SongItem - initial struct

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const SongItem = () => {
   return (
-    <div className='song-item'>
-        SongItem
-    </div>
-  )
-}
+    <Link to="/song/1" className="song-item">
+      <div className="song-item__number-album">
+        <p>{/*index */}</p>
+        <div className="song-item__album">
+          <img src="" alt="Imagem da Música" className="song-item__image" />
+          <p className="song-item__name">{/*Name */}</p>
+        </div>
+      </div>
+      <p>{/*Duration */}</p>
+    </Link>
+  );
+};
 
-export default SongItem
+export default SongItem;
