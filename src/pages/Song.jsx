801bc99b6818283802526689d5ffb7f2fc1b7223
Thomas <thomassantospallarini@gmail.com>
2025-05-09 14:15:20 -0300

    Page: Song - initial struct

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Song = () => {
   return (
-    <div>Song</div>
+    <div className='song'>
+      <div className="song__containter">
+        <div className="song__image-container">
+          <img src="" alt="Imagem da Música" />
+        </div>
+      </div>
+
+      <div className="song__bar">
+        <div className='song__artist-image'>
+          <img src="" alt="Imagem do Artista" />
+        </div>
+        {/*player */}
+        <div>
+          <p className='song__name'>{/*Name */}</p>
+          <p>{/*artist */}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
