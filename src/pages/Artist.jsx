67a06d0f3385b56ff9bc82bf6bb3b918771a40e3
Thomas <thomassantospallarini@gmail.com>
2025-05-09 13:48:12 -0300

    Page: Artist - initial struct

--- src/pages/Artist.jsx
+++ src/pages/Artist.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const Artist = () => {
   return (
-    <div>Artist</div>
-  )
-}
+    <div className="artist">
+      <div
+        className="artist__header"
+        style={{ backgroundImage: "linear-gradient(to bottom, shade, url())" }}
+      >
+        <h2 className="artist__title"></h2>
+      </div>
+      <div className="artist__body">
+        <h2>Populares</h2>
+        {/*<SongList />*/}
+      </div>
 
-export default Artist
+      <Link to="/song/1">
+        <FontAwesomeIcon icon={faCirclePlay} />
+      </Link>
+    </div>
+  );
+};
+
+export default Artist;
