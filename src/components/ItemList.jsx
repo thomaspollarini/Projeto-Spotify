638e62ee3e2e49140c48e440339b3fdde959ab23
Thomas <thomassantospallarini@gmail.com>
2025-05-09 13:26:40 -0300

    Component: Main & ItemList - artists & songs pages

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import SingleItem from "./SingleItem";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const ItemList = ({ title, items, itemsArray, path, idPath }) => {
+  const { pathname } = useLocation();
+  const isHome = pathname === "/";
+  const finalItems = isHome ? items : Infinity;
+
   return (
     <div className="item-list">
       <div className="item-list__header">
         <h2>{title} populares</h2>
-        <Link className="item-list__link" to={path}>
+        {isHome ? <Link className="item-list__link" to={path}>
           Mostrar tudo
-        </Link>
+        </Link> : <></>}
+        
       </div>
       <div className="item-list__container">
         {itemsArray
-          .filter((currentValue, index) => index < items)
+          .filter((currentValue, index) => index < finalItems)
           .map((currObj, index) => (
             <SingleItem
               {...currObj}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,26 +3,27 @@ import ItemList from "./ItemList";
 import { artistArray } from "../assets/database/artists";
 import { songsArray } from "../assets/database/songs";
 
-const Main = () => {
+const Main = ({type}) => {
   return (
     <div className="main">
       {/* Lista de artistas */}
-      <ItemList
+      {type === "artists" || type === undefined ? <ItemList
         title="Artistas"
         items={5}
         itemsArray={artistArray}
         path="/artists"
         idPath="/artist"
-      />
+      /> : <></>}
 
       {/* Lista de músicas */}
-      <ItemList
+      {type === "songs" || type === undefined  ? <ItemList
         title="Músicas"
         items={10}
         itemsArray={songsArray}
         path="/songs"
         idPath="/song"
-      />
+      /> : <></>}
+      
     </div>
   );
 };
