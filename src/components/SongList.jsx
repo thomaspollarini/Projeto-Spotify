6708301c5be869d36498b6bc5782d715d2e66ddb
Thomas <thomassantospallarini@gmail.com>
2025-05-09 19:31:24 -0300

    Component: SongList - state logic -- see more/less

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,14 +1,41 @@
 import React from "react";
 import SongItem from "./SongItem";
+import { useState } from "react";
 
 const SongList = ({ songsArray }) => {
+  const [items, setItems] = useState(5);
+
   return (
     <div className="song-list">
-      {songsArray.map((currSongObj, index) => (
-        <SongItem {...currSongObj} index={index} key={`song-${index}`} />
-      ))}
+      {songsArray
+        .filter((currentValue, index) => index < items)
+        .map((currSongObj, index) => (
+          <SongItem {...currSongObj} index={index} key={`song-${index}`} />
+        ))}
 
-      <p className="song-list__see-more">Ver mais</p>
+      <p
+        className="song-list__see-more"
+        onClick={() => {
+          setItems(items + 5);
+          document.querySelector(".song-list__see-more").style.display = "none";
+          document.querySelector(".song-list__see-less").style.display =
+            "block";
+        }}
+      >
+        Ver mais
+      </p>
+
+      <p
+        className="song-list__see-less"
+        onClick={() => {
+          setItems(items - 5);
+          document.querySelector(".song-list__see-less").style.display = "none";
+          document.querySelector(".song-list__see-more").style.display =
+            "block";
+        }}
+      >
+        Ver menos
+      </p>
     </div>
   );
 };
