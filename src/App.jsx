e495206732942d13616ab1a5f1709525859092cd
Thomas <thomassantospallarini@gmail.com>
2025-05-08 23:34:50 -0300

    App: adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header"
-import Main from "./components/Main";
+import Home from './pages/Home';
+import Artists from './pages/Artists';
+import Artist from './pages/Artist';
+import Songs from './pages/Songs';
+import Song from './pages/Song';
 
-
-function App() {
- 
+const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Main />
-    </>
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/artists" element={<Artists />} />
+        <Route path="/artist/:id" element={<Artist />} />
+        <Route path="/songs" element={<Songs />} />
+        <Route path="/song/:id" element={<Song />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
