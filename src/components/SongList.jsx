f36bc5c6ce78347b3fe276387cf45b016e442ac6
Thomas <thomassantospallarini@gmail.com>
2025-05-09 18:27:53 -0300

    Component: SongList - props

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import SongItem from './SongItem'
+import React from "react";
+import SongItem from "./SongItem";
 
-const SongList = () => {
+const SongList = ({ songsArray }) => {
   return (
-    <div className='song-list'>
-        <SongItem />
+    <div className="song-list">
+      {songsArray.map((currSongObj, index) => (
+        <SongItem {...currSongObj} key={`song-${index}`} />
+      ))}
 
-        <p className='song-list__see-more'>Ver mais</p>
+      <p className="song-list__see-more">Ver mais</p>
     </div>
-  )
-}
+  );
+};
 
-export default SongList
+export default SongList;
