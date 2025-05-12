0903388ecf53e5e5ce0ffb884508242791d7a784
Thomas <thomassantospallarini@gmail.com>
2025-05-12 19:02:06 -0300

    Deploy adjustments

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,22 +1,30 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/api/", (req, res) => {
   res.send("Só vamos trabalhar com os endpoints '/artists' e '/songs'");
 });
 
-app.get("/artists", async(req, res) => {
+app.get("/api/artists", async (req, res) => {
   res.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async(req, res) => {
+app.get("/api/songs", async (req, res) => {
   res.send(await db.collection("songs").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+
+app.get("/:wildcart", (req, res) => {
+  res.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
 });
 
 app.listen(PORT, () => {
--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:3000';
+const URL = import.meta.env.VITE_API_URL;
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
